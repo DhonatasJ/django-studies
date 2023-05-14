89fde49b6ceeaf45deb64ddd936d72b96456bc45
dhonatasj <dhonatascross@gmail.com>
2023-05-14 03:24:51 -0400

    Finish this basic part

--- knowing_mvt/meuprojeto/meuprojeto/settings.py
+++ knowing_mvt/meuprojeto/meuprojeto/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,8 +81,11 @@ WSGI_APPLICATION = 'meuprojeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dhonatas',
+        'USER': 'postgres',
+        'PASSWORD': '4321a',
+        'HOST': 'localhost'
     }
 }
 
@@ -126,5 +130,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
