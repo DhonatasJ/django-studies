6520c0dc5d9c8b444d8d80a0ade1d61aca79be48
Dhonatas Jesus <dhonatascross@gmail.com>
2023-05-24 21:38:58 -0300

    Updates

--- 2-renderingtemplate/signs/polls_signs/views.py
+++ 2-renderingtemplate/signs/polls_signs/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, '')
+    allsigns = {'signs'}
+    return render(request, '/styles/home.html', allsigns)
